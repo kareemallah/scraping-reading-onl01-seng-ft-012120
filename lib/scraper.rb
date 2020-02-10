12e97d6c0cd84a18e1cf27be505d048f7ede22ee
Kareem Rolle <kareemrolle@yahoo.com>
2020-02-10 02:05:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,11 +3,6 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
-=end
-require 'open-uri'
-require 'pry'
-require 'nokogiri' #=> I added this one
-require 'rubygems' #=> I added this one
 class Scraper
 
   def self.scrape_index_page(index_url)
